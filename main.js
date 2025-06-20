bebec688750149e3f1b2a26b13b5f041743d4031
Yaroslav4tez <yaroslav4tez@gmail.com>
2025-06-20 23:19:13 +0200

    change let to const

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 let input = prompt('Your number');
-
+input = input.trim();
 if (input === null || input.trim() === "" || isNaN(Number(input)) || input.startsWith("0") || !/^\d+$/.test(input)) {
     console.log('Please enter only valid natural numbers without leading 0.');
 } else {
-    let n = Number(input);
+    const n = Number(input);
     for (let i = 1; i <= 100; i++) {
-        let sq = i * i;
+        const sq = i * i;
         if (sq <= n) {
             console.log(`Number ${i} its square ${sq}, which is not biges than ${n}`);
         }
