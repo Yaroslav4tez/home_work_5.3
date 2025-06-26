e011954a69e0a8a5a76d7631cc7e4e4880fcc09f
Yaroslav4tez <yaroslav4tez@gmail.com>
2025-06-26 21:37:20 +0200

    fixed

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 let input = prompt('Your number');
-input = input.trim();
-if (input === null || input.trim() === "" || isNaN(Number(input)) || input.startsWith("0") || !/^\d+$/.test(input)) {
+
+if (input === null || input.trim() === "" || !Number.isInteger(Number(input)) || Number(input) <= 0) {
     console.log('Please enter only valid natural numbers without leading 0.');
 } else {
+    input = input.trim();
     const n = Number(input);
     for (let i = 1; i <= 100; i++) {
         const sq = i * i;
         if (sq <= n) {
             console.log(`Number ${i} its square ${sq}, which is not biges than ${n}`);
-        }
-    }
+        } 
+    } 
+    
 }
